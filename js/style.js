9f99f3d4ba0fe80ecfac9d93aecf45ace6d572de
Fthackvin <ft.v@me.com>
2016-11-15 12:50:51 +0100

    developer js

--- js/style.js
+++ js/style.js
@@ -10,26 +10,96 @@ $(document).ready(function(){
 	// TweenMax.to(header1, 3, {opacity: 1, ease:Power4.fadein})
 });
 
+
+
+
 $(document).ready(function(){
 
-	//init ScrollMagic
-	var controller = new ScrollMagic.Controller();
+//Init ScrollMagic
+var controller = new ScrollMagic.Controller();
 
-	// build  scene 
-	var ourScene = new ScrollMagic.Scene({
-		triggerElement: '#developer_image_container',
-		duration: '80%',
-		triggerHook: 0.9,
-		// reverse: false
+// build a scene	
+	var ourScene = new ScrollMagic.Scene ({
+		triggerElement: '#triggertopimage',
+		duration: '100%',
+    	triggerHook: 0.8
 	})
-	.setClassToggle('#computer_layer_1', 'fade-in') // add class to Developer
-	// .addIndicators({
-	// 	colorTrigger:'pink',
-	// 	indent: 100,
-	// 	colorStart:'red',
-
-	// .setTween ("#computer_layer_1,", 0.5, {opacity: 1, y: 10,})
-
+	.setClassToggle('#computer_layer_1', 'fade-in')
+	.addIndicators({
+		name: 'bottom layer',
+		colorTrigger: 'pink',
+		indent: 200,
+		colorstart:'red'
+	})
 	.addTo(controller);
-	var loglevel = controller.loglevel(3);
+
+});
+
+
+$(document).ready(function(){
+
+//Init ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+// build a scene	
+	var ourScene = new ScrollMagic.Scene ({
+		triggerElement:'#computer_layer_4_top',
+		duration: '120%',
+    	triggerHook: 0.79
+	})
+	.setClassToggle('#computer_layer_2', 'fade-in')
+	.addIndicators({
+		name: 'second layer',
+		colorTrigger: 'pink',
+		indent: 250,
+		colorstart:'red'
+	})
+	.addTo(controller);
+
+});
+
+
+$(document).ready(function(){
+
+//Init ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+// build a scene	
+	var ourScene = new ScrollMagic.Scene ({
+		triggerElement: '#computer_layer_2',
+		duration: '140%',
+    	triggerHook: 0.78
+	})
+	.setClassToggle('#computer_layer_3', 'fade-in')
+	.addIndicators({
+		name: 'third layer',
+		colorTrigger: 'pink',
+		indent: 300,
+		colorstart:'red'
+	})
+	.addTo(controller);
+
+});
+
+
+$(document).ready(function(){
+
+//Init ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+// build a scene	
+	var ourScene = new ScrollMagic.Scene ({
+		triggerElement: '#computer_layer_1',
+		duration: '160%',
+    	triggerHook: 0.76
+	})
+	.setClassToggle('#computer_layer_4_top', 'fade-in')
+	.addIndicators({
+		name: 'top layer',
+		colorTrigger: 'pink',
+		indent: 350,
+		colorstart:'red'
+	})
+	.addTo(controller);
+
 });
